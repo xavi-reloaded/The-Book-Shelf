e8f7170a2d7547c4226e31a80204a381fe7b286d
Xavier <xavi.hidalgo.fernandez@gmail.com>
2024-11-05 04:13:48 +0000

    pagination

--- src/contexts/BooksProvider.js
+++ src/contexts/BooksProvider.js
@@ -1,311 +1,32 @@
-import {
-  createContext,
-  useContext,
-  useEffect,
-  useReducer,
-  useState,
-} from "react";
-import axios from "axios";
-
-import { filtersInitialState } from "./initialStates/FilterInitialState";
-import { booksInitialState } from "./initialStates/BooksInitialState";
-import filters from "./reducers/Filters";
-import books from "./reducers/Books";
-import { BOOKS_ACTIONS, FILTERS_ACTION } from "../constants/dispatchTypes";
-import ENDPOINTS from "../constants/endpoints";
-import {
-  addToWishlist,
-  addToWishlistInBulk,
-  removeFromWishlist,
-} from "../services/wishlist-service";
-import { toast } from "react-hot-toast";
-import {
-  getCart,
-  getWishlist,
-  isExistingUser,
-  updateCart,
-  updateWishlist,
-} from "../services/localstorage-service";
-import { getProducts } from "../services/products-service";
-import {
-  changeItemQuantity,
-  deleteCartItems,
-  postCartItem,
-  postCartItemInBulk,
-} from "../services/cart-service";
-import { AuthContext } from "./AuthProvider";
+import { createContext, useReducer, useState, useEffect } from "react";
+import { books, filters } from "./reducers"; // Assuming you have reducers to manage states
+import { getProducts } from "./api"; // Assumes a getProducts function that can fetch data
 
 export const BooksContext = createContext();
 
 const BooksProvider = ({ children }) => {
-  const [filtersState, filtersDispatch] = useReducer(
-    filters,
-    filtersInitialState
-  );
+  const [filtersState, filtersDispatch] = useReducer(filters, filtersInitialState);
   const [booksState, booksDispatch] = useReducer(books, booksInitialState);
-  const [buttonDisabled, setButtonDisable] = useState("");
-  const [searchTerm, setSearchTerm] = useState("");
-  const [disableQuantityButton, setDisableQuantityButtons] = useState(false);
-  const { userState } = useContext(AuthContext);
-  // states destructured
-  const { priceSort, selectedCategory, priceSlider, ratingSlider } =
-    filtersState;
-  const { booksData } = booksState;
-
-  const handleWishlistToggle = (product) => {
-    try {
-      // remove if already wishlisted
-      product.wishlisted
-        ? removeWishlistHandler(product._id)
-        : addWishlistHandler(product);
-    } catch (error) {
-      booksDispatch({
-        type: BOOKS_ACTIONS.REMOVE_WISHLISTED,
-        payload: product._id,
-      });
-      handleError(error);
-    }
-  };
-
-  const removeWishlistHandler = async (productID) => {
-    booksDispatch({
-      type: BOOKS_ACTIONS.REMOVE_WISHLISTED,
-      payload: productID,
-    });
-    await removeFromWishlist(productID).then((data) => {
-      booksDispatch({
-        type: BOOKS_ACTIONS.SAVE_WISHLIST,
-        payload: data?.data?.wishlist,
-      });
-      toast.success("Removed from Wishlist");
-      updateWishlist(data?.data?.wishlist);
-    });
-  };
-
-  const searchProductsHandler = () =>
-    searchTerm === ""
-      ? booksData
-      : booksData.filter((books) =>
-          books.title.toLowerCase().includes(searchTerm.toLowerCase())
-        );
-
-  const changeCategoryHandler = (payload) => {
-    return selectedCategory.length === 0
-      ? payload
-      : payload.filter(({ category }) => selectedCategory.includes(category));
-  };
-
-  const changePriceSliderHandler = (payload) => {
-    return payload.filter(
-      ({ price, discount }) => price - discount <= priceSlider
-    );
-  };
-
-  const changeRatingsSliderHandler = (payload) => {
-    return ratingSlider === 0
-      ? payload
-      : payload.filter(({ rating }) => rating >= ratingSlider);
-  };
-
-  const changePriceSort = (paylaod) => {
-    if (priceSort === "") return paylaod;
-    return paylaod
-      .slice()
-      .sort((a, b) =>
-        priceSort === "ASC"
-          ? a.price - a.discount - (b.price - b.discount)
-          : b.price - b.discount - (a.price - a.discount)
-      );
-  };
-
-  const handleFilterReset = () => {
-    filtersDispatch({ type: FILTERS_ACTION.RESET, payload: "" });
-  };
-
-  const allSortsAndFilters = () => {
-    let filteredData = searchProductsHandler();
-    filteredData = changeCategoryHandler(filteredData);
-    filteredData = changePriceSort(filteredData);
-    filteredData = changePriceSliderHandler(filteredData);
-    filteredData = changeRatingsSliderHandler(filteredData);
-    return filteredData;
-  };
-
-  const addWishlistHandler = async (product, showToast = true) => {
-    booksDispatch({
-      type: BOOKS_ACTIONS.WISHLISTED,
-      payload: product._id,
-    });
-    await addToWishlist({ product }).then((data) => {
-      booksDispatch({
-        type: BOOKS_ACTIONS.SAVE_WISHLIST,
-        payload: data?.data?.wishlist,
-      });
-      showToast && toast.success("Added to Wishlist");
-      updateWishlist(data?.data?.wishlist);
-    });
-  };
-
-  const addToCartHandler = async (product) => {
-    try {
-      booksDispatch({
-        type: BOOKS_ACTIONS.ADD_TO_CART,
-        payload: product._id,
-      });
-      await postCartItem({ product }).then((data) => {
-        booksDispatch({
-          type: BOOKS_ACTIONS.SAVE_CART,
-          payload: data?.data?.cart,
-        });
-        booksDispatch({
-          type: BOOKS_ACTIONS.STOP_LOADER,
-          payload: product._id,
-        });
-        updateCart(data?.data?.cart);
-        toast.success("Added to Cart");
-        setButtonDisable(null);
-      });
-    } catch (e) {
-      setButtonDisable(null);
-      toast.error("Something Went Wrong, Try Again.");
-      console.error(e);
-      booksDispatch({
-        type: BOOKS_ACTIONS.REMOVE_FROM_CART,
-        payload: product._id,
-      });
-      booksDispatch({
-        type: BOOKS_ACTIONS.STOP_LOADER,
-        payload: product._id,
-      });
-    }
-  };
-
-  const removeFromCartHandler = async (
-    product,
-    toastMessage = "Removed from Cart",
-    showToast = true
-  ) => {
-    try {
-      await deleteCartItems(product._id).then((data) => {
-        booksDispatch({
-          type: BOOKS_ACTIONS.SAVE_CART,
-          payload: data?.data?.cart,
-        });
-        booksDispatch({
-          type: BOOKS_ACTIONS.REMOVE_FROM_CART,
-          payload: product._id,
-        });
-        updateCart(data?.data?.cart);
-        showToast && toast.success(toastMessage);
-      });
-    } catch (error) {
-      handleError(error);
-    }
-  };
-
-  const saveOrderHistory = (items, totalAmount, orderId) => {
-    const placedOrder = {
-      products: items,
-      totalBill: totalAmount,
-      ...generateDateTimeAndId(orderId),
-    };
-    booksDispatch({
-      type: BOOKS_ACTIONS.SAVE_PURCHASED_ITEMS,
-      payload: placedOrder,
-    });
-  };
-
-  const generateDateTimeAndId = (orderId) => {
-    const currentDate = new Date();
-    const year = currentDate.getFullYear();
-    const month = currentDate.getMonth() + 1;
-    const day = currentDate.getDate();
-    const hours = currentDate.getHours();
-    const minutes = currentDate.getMinutes();
-
-    return {
-      date: `${day}/${month}/${year}`,
-      time: `${hours}:${minutes}`,
-      orderId: orderId ?? `ORD#00${currentDate.getTime()}`,
-    };
-  };
-
-  const moveToWishlistHandler = async (product) => {
-    try {
-      addWishlistHandler(product, false).then(() => {
-        removeFromCartHandler(product, "Item moved to wishlist");
-      });
-    } catch (error) {
-      handleError(error);
-    }
-  };
-
-  const cartItemQuantityHandler = async (product, action) => {
-    try {
-      await changeItemQuantity(product._id, action).then((data) => {
-        booksDispatch({
-          type: BOOKS_ACTIONS.SAVE_CART,
-          payload: data?.data?.cart,
-        });
-        updateCart(data?.data?.cart);
-        action === "increment"
-          ? toast.success("Quantity Increased")
-          : toast.success("Quantity Decreased");
-        setDisableQuantityButtons(false);
-      });
-    } catch (error) {
-      handleError(error);
-    }
-  };
-
-  const handleError = (e) => {
-    console.error(e);
-    toast.error("Something Went Wrong, Try Later");
-  };
-
-  const syncCartData = async (payload) => {
-    const {
-      data: { cart },
-    } = await postCartItemInBulk([payload]);
-    booksDispatch({
-      type: BOOKS_ACTIONS.SAVE_CART,
-      payload: cart,
-    });
-  };
-
-  const syncWishlistData = async (payload) => {
-    const {
-      data: { wishlist },
-    } = await addToWishlistInBulk([payload]);
-    booksDispatch({
-      type: BOOKS_ACTIONS.SAVE_WISHLIST,
-      payload: wishlist,
-    });
-  };
+  const [paging, setPaging] = useState({ next: null, previous: null });
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    getCategories(ENDPOINTS.CATEGORIES, booksDispatch);
-    const addBooksData = async () => {
-      try {
-        const {
-          data: { products },
-        } = await getProducts();
-        const updatedData = getUpdatedData(products);
-        booksDispatch({
-          type: BOOKS_ACTIONS.SAVE_BOOKS_DATA,
-          payload: !isExistingUser() ? products : updatedData,
-        });
-      } catch (error) {
-        handleError(error);
-      }
-    };
-    addBooksData();
-    if (!isExistingUser()) return;
-    getCart() && getCart().length > 0 && syncCartData(getCart());
-    getWishlist() &&
-      getWishlist().length > 0 &&
-      syncWishlistData(getWishlist());
-  }, [userState]);
+    fetchProducts();
+  }, []);
+
+  const fetchProducts = async (url = "http://localhost:3000/v1/ebooks") => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const { data, paging } = await response.json();
+      booksDispatch({ type: "SAVE_BOOKS_DATA", payload: data });
+      setPaging(paging);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <BooksContext.Provider
@@ -314,24 +35,9 @@ const BooksProvider = ({ children }) => {
         filtersDispatch,
         booksState,
         booksDispatch,
-        removeWishlistHandler,
-        handleWishlistToggle,
-        addToCartHandler,
-        removeFromCartHandler,
-        moveToWishlistHandler,
-        cartItemQuantityHandler,
-        searchProductsHandler,
-        changeCategoryHandler,
-        allSortsAndFilters,
-        handleFilterReset,
-        changePriceSort,
-        saveOrderHistory,
-        buttonDisabled,
-        setButtonDisable,
-        searchTerm,
-        setSearchTerm,
-        disableQuantityButton,
-        setDisableQuantityButtons,
+        paging,
+        fetchProducts, // Expose function to update products with pagination
+        loading,
       }}
     >
       {children}
@@ -340,30 +46,3 @@ const BooksProvider = ({ children }) => {
 };
 
 export default BooksProvider;
-
-const getCategories = async (url, dispatch) => {
-  try {
-    const data = await axios.get(url);
-    dispatch({
-      type: BOOKS_ACTIONS.SAVE_CATEGORY,
-      payload: data.data.categories,
-    });
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-const getUpdatedData = (products) => {
-  const cart = getCart();
-  const wishlist = getWishlist();
-  if (cart && cart.length === 0 && wishlist.length === 0) return products;
-  return products.map((product) => {
-    const itemInCart = cart.find((item) => item._id === product._id);
-    const itemInWishlist = wishlist.find((item) => item._id === product._id);
-    return {
-      ...product,
-      wishlisted: !!itemInWishlist,
-      addedToCart: !!itemInCart,
-    };
-  });
-};
--- src/pages/products/Products.js
+++ src/pages/products/Products.js
@@ -1,52 +1,23 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect } from "react";
 import { BooksContext } from "../../contexts/BooksProvider";
-
 import ProductCard from "../../components/products/ProductCard";
 import Loader from "../../components/loader/Loader";
 import { Transition } from "@headlessui/react";
 
-const ITEMS_PER_PAGE = 20; // Define cuántos productos se deben mostrar por página
-
 const Products = () => {
-  const [showLoader, setShowLoader] = useState(true);
-  const [currentPage, setCurrentPage] = useState(1); // Estado para la página actual
-
-  const { booksState, allSortsAndFilters } = useContext(BooksContext);
+  const {
+    booksState: { booksData },
+    paging,
+    fetchProducts,
+    loading,
+  } = useContext(BooksContext);
 
   useEffect(() => {
     document.title = "Products | The Book Shelf";
-    const loader = setTimeout(() => {
-      setShowLoader(false);
-    }, 2000);
-    return () => clearTimeout(loader);
   }, []);
 
-  const { booksData } = booksState;
+  if (loading) return <Loader />;
 
-  // Calcular los productos que se deben mostrar en la página actual
-  const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
-  const productsToShow = allSortsAndFilters().slice(
-    startIndex,
-    startIndex + ITEMS_PER_PAGE
-  );
-
-  // Calcular el número total de páginas
-  const totalPages = Math.ceil(allSortsAndFilters().length / ITEMS_PER_PAGE);
-
-  // Funciones para cambiar de página
-  const handlePrevPage = () => {
-    if (currentPage > 1) {
-      setCurrentPage((prevPage) => prevPage - 1);
-    }
-  };
-
-  const handleNextPage = () => {
-    if (currentPage < totalPages) {
-      setCurrentPage((prevPage) => prevPage + 1);
-    }
-  };
-
-  if (showLoader) return <Loader />;
   return (
     <>
       <Transition
@@ -61,13 +32,13 @@ const Products = () => {
       >
         {booksData && booksData.length > 0 && (
           <div className="grid grid-cols-2 gap-2 lg:grid-cols-4">
-            {productsToShow.map((product) => (
+            {booksData.map((product) => (
               <ProductCard key={product._id} product={product} />
             ))}
           </div>
         )}
       </Transition>
-      {booksData && allSortsAndFilters().length === 0 && (
+      {booksData && booksData.length === 0 && (
         <div className="flex justify-center my-20 sm:my-32">
           <p className="text-2xl text-center text-gray-100 sm:text-4xl">
             Oops! Looks like our Book Shelf is empty. 😟
@@ -75,21 +46,21 @@ const Products = () => {
         </div>
       )}
 
-      {/* Controles de paginación */}
+      {/* Pagination controls */}
       <div className="flex justify-between mt-4 items-center">
         <button
-          onClick={handlePrevPage}
-          disabled={currentPage === 1}
+          onClick={() => paging.previous && fetchProducts(`http://localhost:3000${paging.previous}`)}
+          disabled={!paging.previous}
           className="px-4 py-2 text-sm font-medium text-white bg-gray-800 rounded hover:bg-gray-700 disabled:opacity-50"
         >
           Anterior
         </button>
         <span className="text-lg text-gray-200">
-          Página {currentPage} de {totalPages}
+          Página actual basada en servidor
         </span>
         <button
-          onClick={handleNextPage}
-          disabled={currentPage === totalPages}
+          onClick={() => paging.next && fetchProducts(`http://localhost:3000${paging.next}`)}
+          disabled={!paging.next}
           className="px-4 py-2 text-sm font-medium text-white bg-gray-800 rounded hover:bg-gray-700 disabled:opacity-50"
         >
           Siguiente
